db8ffebdeafef9acd4bbb2fd4a24ba00a2bcdcfc
arraytool <srinivas.zinka@gmail.com>
2011-01-31 21:41:37 +0900

    nothing important

--- arraytool/src/examples/test.py
+++ arraytool/src/examples/test.py
@@ -1,1 +1,13 @@
 #! /usr/bin/env python
+
+#def z_f(a, b, m):
+#    """Summation of the series (eq.(23),[1])"""
+#    K = ellipk(m)
+#    K1 = ellipk(1 - m)
+#    q1 = mp.exp(-mp.pi * K / K1)
+#    NM = mp.cosh((a + b) * mp.pi / (2 * K))
+#    DM = mp.cosh((a - b) * mp.pi / (2 * K))
+#    fun = lambda r: ((-1) ** r / r) * ((q1 ** (2 * r)) / (1 - q1 ** (2 * r)))\
+#     * mp.sinh(mp.pi * r * a / K1) ** mp.sinh(mp.pi * r * b / K1)
+#    f = -(mp.pi * a * b / (K * K1)) + mp.log(NM / DM) - 4 * nsum(fun, [1, inf])
+#    return f
--- arraytool/src/examples/zolotarev.py
+++ arraytool/src/examples/zolotarev.py
@@ -6,12 +6,14 @@ Zolotarev array distribution. (not finished yet)
 References:
 [1] McNamara, D. A., "Direct synthesis of optimum difference patterns for 
     discrete linear arrays using Zolotarev distributions", 
-    IEE Proceedings H Microwaves, Antennas and Propagation, 1993, 140, 495-500 
+    IEE Proceedings H Microwaves, Antennas and Propagation, 1993, 140, 495-500
+[2] Abramowitz and Stegun, "Handbook of Mathematical Functions", Dover, 1964,
+    http://people.math.sfu.ca/~cbm/aands/frameindex.htm, Page 577
 """
 
 from __future__ import division
 from mpmath import mpf, mp, qfrom, ellipk, ellipe, ellipf, ellipfun, jtheta, \
-                   nsum, inf, elliprf
+                   nsum, inf, mpc
 
 #==============================================================================
 # Precision level
@@ -24,59 +26,72 @@ mp.dps = 25; mp.pretty = True
 #==============================================================================
 
 def z_theta(u, m):
-    """Jacobi theta (\Theta or B as depicted in [1]) function."""
+    """Jacobi theta function (eq 16.31.1, [2, p.577])."""
     q = qfrom(m=m)
     DM = ellipk(m)
     z = mp.pi * u / (2 * DM)
-    theta = jtheta(n=4, z=z, q=q, derivative=0)
+    theta = jtheta(n=4, z=z, q=q)
     return theta
 
 def z_eta(u, m):
-    """Jacobi eta (H) function [1]."""
+    """Jacobi eta function (eq 16.31.3, [2, p.577])."""
     q = qfrom(m=m)
     DM = ellipk(m)
     z = mp.pi * u / (2 * DM)
-    eta = jtheta(n=1, z=z, q=q, derivative=0)
+    eta = jtheta(n=1, z=z, q=q)
     return eta
 
-def z_x123fromm(N, m):
-    """Function to get x2, where Zolotarev polynomial is 
-       maximum/minimum in region II [1]."""
-    # argument
+def z_am(u, m):
+    """Jacobi amplitude function (eq 16.1.5, [2, p.569])."""
+    snM = ellipfun('sn', u=u, m=m)
+    cnM = ellipfun('cn', u=u, m=m)
+    if (0<=cnM<=1):
+        phi = mp.asin(snM)
+    elif (-1<=cnM<0):
+        if (snM>=0):
+            phi = mp.pi - mp.asin(snM)
+        else:
+            phi = mp.asin(snM) - mp.pi
+    else:
+        print "This function only handles real 'phi' values."
+    return phi
+
+def z_zn(u, m):
+    """Jacobi Zeta (zn(u,m)) function (eq 16.26.12, [2, p.576])."""
+    phi = z_am(u, m)
+    zn = ellipe(phi, m) - ellipe(m)*u/ellipk(m)
+    return zn
+
+def z_x123from_m(N, m):
+    """Function to get x1, x2 and x3 (eq 3, 5 and 6, [1])."""
     M = -ellipk(m) / N
-    # evaluation of 'x2' value for given N, k values
     snMM = ellipfun('sn', u= -M, m=m)
     snM = ellipfun('sn', u=M, m=m)
     cnM = ellipfun('cn', u=M, m=m)
     dnM = ellipfun('dn', u=M, m=m)
-    phi = mp.asin(snM) # decide what arcsin u need to take ... understand Jacobi amplitude
-    znM = ellipe(phi, m) - ellipe(m) * ellipf(phi, m) / ellipk(m)
+    znM = z_zn(M, m)
     x3 = snMM
     x1 = x3 * mp.sqrt(1 - m) / dnM
     x2 = x3 * mp.sqrt(1 - (cnM * znM) / (snM * dnM))  
     return x1, x2, x3
 
-def z_f(a, b, m):
-    """Summation of the series (eq.(23),[1])"""
-    K = ellipk(m)
-    K1 = ellipk(1 - m)
-    q1 = mp.exp(-mp.pi * K / K1)
-    NM = mp.cosh((a + b) * mp.pi / (2 * K))
-    DM = mp.cosh((a - b) * mp.pi / (2 * K))
-    fun = lambda r: ((-1) ** r / r) * ((q1 ** (2 * r)) / (1 - q1 ** (2 * r)))\
-     * mp.sinh(mp.pi * r * a / K1) ** mp.sinh(mp.pi * r * b / K1)
-    f = -(mp.pi * a * b / (K * K1)) + mp.log(NM / DM) - 4 * nsum(fun, [1, inf])
-    return f
-
 def z_zolotarev2(N, x, m):
     """Value of the Zolotarev polynomial in region II [1]."""    
     # argument (eq.(4),[1])
     M = -ellipk(m) / N
-    snM = ellipfun('sn', u=M, m=m)    
+    snM = ellipfun('sn', u=M, m=m)  
+    
+    
+    # Something WRONG here
+      
     # evaluation of p (eq.(24),[1])
     p = mp.sqrt((snM ** 2 - x ** 2) / (m * snM ** 2 * (1 - x ** 2))) 
+    p = mp.acos(p)
+    
+    
+    
     # evaluation of s
-    s = ellipf(p, m) # convert p to Jacobi amplitude 'phi'  and # decide what arcsin u need to take ... understand Jacobi amplitude
+    s = ellipf(p, m)
     # evaluation of 'f(M,s,m)'
     fMsm = mp.log(z_theta(M + s, m) / z_theta(M - s, m))    
     # finally, the value of the Zolotarev polynomial  (eq.(21),[1])
@@ -84,17 +99,22 @@ def z_zolotarev2(N, x, m):
     return f
 
 #==============================================================================
-# Checking SLR value
+# Basic parameters
 #==============================================================================
 
-# various parameters
 n = 9
 N = 2 * n + 1
 k = mpf('0.999895316')
 #k = mpf('0.9')
 m = k ** 2 # MODULUS 'k' is not convenient, so we use PARAMETER 'm' instead
+
+#==============================================================================
+# Checking SLR value
+#==============================================================================
+
 #  x1, x2 and x3 positions [1]
-x1, x2, x3 = z_x123fromm(N, m)
+x1, x2, x3 = z_x123from_m(N, m)
+print x1, '\n', x2, '\n', x3
 x = x2
 # value of Zolotarev polynomial in region II
 R = z_zolotarev2(N, x, m)
@@ -105,25 +125,25 @@ print 10 * mp.log10(R ** 2) # SLR depends only on the magnitude of R here
 # Checking function f
 #==============================================================================
 
-# argument (eq.(4),[1])
-M = -ellipk(m) / N
-#print M
-## ellipk in-terms of elliprf
-#M = -elliprf(0, 1-m, 1) / N
-#print M
-snM = ellipfun('sn', u=M, m=m)    
-# evaluation of p (eq.(24),[1])
-p = mp.sqrt((snM ** 2 - x ** 2) / (m * snM ** 2 * (1 - x ** 2)))    
-p = mp.acos(p)
-# evaluation of s
-p = 4
-m = mpf(2)/3
-snp = ellipfun('sn', u=p, m=m)   
-phi_p = 1*mp.pi - mp.asin(snp) # decide what arcsin u need to take
-print phi_p
-s = ellipf(p, m)  
-
-fMsm = z_f(M, s, m)
-R = mp.cos(n * mp.pi) * mp.cosh((n + 0.5) * fMsm)        
-print R
-#print 10 * mp.log10(R ** 2) # SLR depends only on the magnitude of R here
+## argument (eq.(4),[1])
+#M = -ellipk(m) / N
+##print M
+### ellipk in-terms of elliprf
+##M = -elliprf(0, 1-m, 1) / N
+##print M
+#snM = ellipfun('sn', u=M, m=m)    
+## evaluation of p (eq.(24),[1])
+#p = mp.sqrt((snM ** 2 - x ** 2) / (m * snM ** 2 * (1 - x ** 2)))    
+#p = mp.acos(p)
+## evaluation of s
+#p = 4
+#m = mpf(2)/3
+#snp = ellipfun('sn', u=p, m=m)   
+#phi_p = 1*mp.pi - mp.asin(snp) # decide what arcsin u need to take
+#print phi_p
+#s = ellipf(p, m)  
+#
+#fMsm = z_f(M, s, m)
+#R = mp.cos(n * mp.pi) * mp.cosh((n + 0.5) * fMsm)        
+#print R
+##print 10 * mp.log10(R ** 2) # SLR depends only on the magnitude of R here
